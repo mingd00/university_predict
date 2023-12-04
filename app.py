038bcab1a70063039340265cf8134c5176f40f4d
mingd00 <rkdalswl403@naver.com>
2023-12-04 20:25:27 +0900

    [feat] Create Model & Test

--- app.py
+++ app.py
@@ -1,40 +1,36 @@
-import pandas as pd
+from flask import Flask, request, jsonify
+from keras.models import load_model
 import numpy as np
-import tensorflow as tf
 
-data = pd.read_csv('gpascore.csv')
+model = load_model('model/best_model.h5')
 
-# 데이터 전처리(결측치 처리)
-# print(data.isnull().sum()) 빈칸의 개수를 보여줌
-data = data.dropna()  # 빈값이 있는 행을 제거
-# print(data.isnull().sum())
-# data = data.fillna(100) 빈칸을 내가 원하는 값으로 채워줌
+app = Flask(__name__)
 
-x = []
-y = data['admit'].values
+@app.route('/')
+def home():
+    return "Hello World"
 
-# data라는 데이터 프레임을 가로 한 줄씩 출력해 주라
-for i, rows in data.iterrows():
-    x.append([rows['gre'], rows['gpa'], rows['rank']])
+@app.route('/predict', methods=['POST'])
+def predict():
+    #데이터 받을 빈 리스트 생성
+    data_list = []  
+
+    #사용자가 입력한 값 불러오기
+    data_list = request.get_json()['data_list']
+
+    #데이터 형태 맞춰주기
+    input_query = np.array([data_list], dtype=np.float32)
+
+	#모델과 비교
+    result = model.predict(input_query)[0]
     
-print(x)
-    
+    # 백분위로 변환하고 소수점 뒤에 숫자 없애기
+    result = int(result[0] * 100)
 
-model = tf.keras.models.Sequential([
-    # 숫자 같은 경우 계속 돌려 보면서 정확도가 높은 방향으로 모델을 튜닝시켜 주는 작업이 필요함
-    tf.keras.layers.Dense(64, activation='tanh'),
-    tf.keras.layers.Dense(128, activation='tanh'),
-    # 마지막 레이어는 예측결과(sigmoid -> 0~1확률)
-    tf.keras.layers.Dense(1, activation='sigmoid'),
-])
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# 학습 데이터, 실제 정답
-model.fit(np.array(x), np.array(y), epochs=1000)
-
-# 예측
-predict = model.predict([ [750, 3.70, 3], [400, 2.2, 1] ])
-print(predict)
+    #json 형식으로 return
+    return jsonify({'합격 확률' : f'{result}%'})
 
 
+if __name__ == '__main__':
+    # Run Flask app
+    app.run(debug=True, port=8080)
